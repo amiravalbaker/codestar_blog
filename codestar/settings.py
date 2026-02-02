5084dd3a45a870ae38b5502cda6f0e3353032edd
amiravalbaker <amiravb@yahoo.co.uk>
2026-02-02 12:00:11 +0000

    add navbar link

--- codestar/settings.py
+++ codestar/settings.py
@@ -20,16 +20,20 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -83,7 +87,6 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
-
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
