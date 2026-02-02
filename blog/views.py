b012c2568301570080456b2aca0c93f91bbae10a
amiravalbaker <amiravb@yahoo.co.uk>
2026-02-02 16:11:48 +0000

    fix indentation error in views

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,13 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
     
-    
-    def post_detail(request, slug):
+def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
 
@@ -28,4 +26,8 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
-    return render(request,"blog/post_detail.html",{"post": post},)
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+        )
