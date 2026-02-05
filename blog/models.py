04582a142e62f45de9090d9981285640fd0d3260
amiravalbaker <amiravb@yahoo.co.uk>
2026-02-05 13:18:28 +0000

    image file functionality update

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
 )
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
